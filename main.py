1de8ef7b0a7f596e1f0c72d2efe32c7520eb675f
Jazmin CamG <jazmin.camacho7450@alumnos.udg.mx>
2020-09-28 12:38:20 -0500

    Prueba 1 zodiaco

--- main.py
+++ main.py
@@ -1,1 +1,62 @@
-print("Hola Mundo")
+dia=int(input())
+mes=int(input())
+if mes == 1:
+    if dia < 20:
+        print("Capricornio")
+    if dia >= 20:
+        print("Acuario")
+elif mes == 2:
+    if dia < 19:
+        print("Acuario")
+    if dia >= 19:
+        print("Piscis")
+elif mes == 3:
+    if dia < 21:
+        print("Piscis")
+    if dia >= 21:
+        print("Aries")
+elif mes == 4:
+    if dia < 20:
+        print("Aries")
+    if dia >= 20:
+        print("Tauro")
+elif mes == 5:
+    if dia < 21:
+        print("Tauro")
+    if dia >= 21:
+        print("Geminis")
+elif mes == 6:
+    if dia < 21:
+        print("Geminis")
+    if dia >= 21:
+        print("Cancer")
+elif mes == 7:
+    if dia < 23:
+        print("Cancer")
+    if dia >= 23:
+        print("Leo")
+elif mes == 8:
+    if dia < 23:
+        print("Leo")
+    if dia >= 23:
+        print("Virgo")
+elif mes == 9:
+    if dia < 23:
+        print("Virgo")
+    if dia >= 23:
+        print("Libra")
+elif mes == 10:
+    if dia < 23:
+        print("Libra")
+    if dia >= 23:
+        print("Escorpio")
+elif mes == 11:
+    if dia < 22:
+        print("Escorpio")
+    if dia >= 22:
+        print("Sagitario")
+elif mes == 12:
+    if dia < 22:
+        print("Sagitario")
+    if dia >= 22:
+        print("Capricornio")
